1dacc5ebb089266e8c48ae43cb8940ff58569a67
59181 <591818880@qq.com>
2018-08-06 17:40:26 +0800

    引入aop记录日志

--- src/main/java/com/xiemj/controller/TestController.java
+++ src/main/java/com/xiemj/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.xiemj.controller;
 
+import com.xiemj.common.annotation.Log;
 import com.xiemj.common.base.BaseController;
 import com.xiemj.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,14 @@ public class TestController extends BaseController {
     private TestService testService;
 
     @RequestMapping("/add")
+    @Log("调用的是add")
     public ResponseEntity<String> add( ){
         Map<String,Object> reusltMap = testService.queryList();
         return super.toJsonResponse(reusltMap);
     }
 
     @RequestMapping("/add1")
+    @Log("调用的是add1")
     public ResponseEntity<String> add1( ){
         Map<String,Object> reusltMap = testService.queryList1();
         return super.toJsonResponse(reusltMap);
